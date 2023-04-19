04558b409f0d4520b5507193b66c6269a006907a
Daniel Ives <daives@colostate.edu>
2023-04-18 21:10:02 -0600

    try to build a larger model

--- nanoGPT/config/train_config.py
+++ nanoGPT/config/train_config.py
@@ -1,13 +1,12 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-nikhil-gpt-first-try'
-eval_interval = 250 # keep frequent because we'll overfit
+eval_interval = 100 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
-always_save_checkpoint = False
+always_save_checkpoint = False 
 
 wandb_log = False # override via command line if you like
 wandb_project = 'nikhil-gpt'
